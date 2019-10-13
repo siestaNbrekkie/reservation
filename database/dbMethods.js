d58486da61d4657b93b01260eaa9d86a3ee4cbf7
Faith <pfaithmtan@gmail.com>
2019-10-12 22:57:49 -0700

    Query database to obtain listings

---
+++ database/dbMethods.js
@@ -0,0 +1,13 @@
+const db = require('./index.js');
+
+module.exports = {
+    getListings: (req, res) => {
+        db.Listing.findAll()
+            .then(data => {
+                res.status(200).send(data);
+            })
+            .catch(err => {
+                res.status(500).send(err);
+            })
+    }
+};
