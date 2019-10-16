bade7ccefcd7a19b2f3720777704030ff595584b
Faith <pfaithmtan@gmail.com>
2019-10-15 18:36:21 -0700

    Fix syntax errors according to  Airbnb style guide
    - Make package-lock.json available

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,26 @@
 const path = require('path');
+
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-    entry: `${SRC_DIR}/index.js`,
-    output: {
-        filename: 'bundle.js',
-        path: DIST_DIR
-    },
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            }
-        ]
-    },
-    
-    resolve: {
-        extensions: ['.js', '.jsx']
-    }
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 };
