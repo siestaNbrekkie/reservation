3ea8a646e2961fdf09f090404200db3051d736f5
Faith <pfaithmtan@gmail.com>
2019-10-17 12:51:02 -0700

    Make GET request to obtain listings

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Calendar from './Calendar';
 import Guest from './Guest';
 import ReserveButton from './ReserveButton';
@@ -7,12 +8,50 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      listingInfo: [],
+    };
   }
 
+  componentDidMount() {
+    this.getListings();
+  }
+
+  getListings() {
+    axios.get('/listings')
+      .then((response) => {
+        this.setState({
+          listingInfo: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+
   render() {
+    // const { listingInfo } = this.state;
+    // const firstListing = listingInfo[0];
+    const firstListing = this.state.listingInfo[0];
+    // console.log('inside render', firstListing);
+    // console.log(firstListing.pricePerNight);
+    console.log(firstListing);
+
     return (
       <div>
+        <div>
+          $
+          {/* {firstListing.pricePerNight} */}
+          /night
+        </div>
+        <div>
+          $
+          {/* {firstListing.ratings} */}
+          (
+          {/* {firstListing.reviews} */}
+          reviews)
+        </div>
         <Calendar />
         <Guest />
         <ReserveButton />
