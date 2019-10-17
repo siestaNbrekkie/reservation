9159f4bf7a80dd778965d73735870fd13ec2efa6
Faith <pfaithmtan@gmail.com>
2019-10-17 14:49:16 -0700

    Change API route
    - Obtain specific listing that user requests
    - Include /api in endpoint to adhere to best practises

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
 
-app.get('/listings', controllers.getListings);
+app.get('/api/rooms/:id', controllers.getListing);
 app.get('/dates', controllers.getDates);
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
