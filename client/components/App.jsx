39250d6227f78ff91b70d085dea0e8d33d4262a6
Faith <pfaithmtan@gmail.com>
2019-10-21 03:58:28 -0700

    Add star

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -69,18 +69,20 @@ class App extends React.Component {
         <DetailsDiv>
           <div>
             <Details style={{ fontWeight: 'bold', fontSize: '22px' }}>
-            $
+              $
             {listingInfo.pricePerNight}
             </Details>
             <Details style={{ fontWeight: '600', fontSize: '12px' }}>
               {'   '}
-            per night
+              per night
             </Details>
           </div>
           <div>
             <Details style={{ fontWeight: 'bold', fontSize: '12px' }}>
-            *
-            {listingInfo.ratings}
+              <img style={{ width: '9px', height: '9px' }}
+                src="data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PScwIDAgMTAwMCAxMDAwJyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIGZpbGw9JyMwMDg0ODknPjxwYXRoIGQ9J005NzIgMzgwYzkgMjggMiA1MC0yMCA2N0w3MjUgNjE5bDg3IDI4MGMxMSAzOS0xOCA3NS01NCA3NS0xMiAwLTIzLTQtMzMtMTJMNDk5IDc5MCAyNzMgOTYyYTU4IDU4IDAgMCAxLTc4LTEyIDUwIDUwIDAgMCAxLTgtNTFsODYtMjc4TDQ2IDQ0N2MtMjEtMTctMjgtMzktMTktNjcgOC0yNCAyOS00MCA1Mi00MGgyODBsODctMjc5YzctMjMgMjgtMzkgNTItMzkgMjUgMCA0NyAxNyA1NCA0MWw4NyAyNzdoMjgwYzI0IDAgNDUgMTYgNTMgNDB6Jy8+PC9zdmc+"
+              />
+              {` ${listingInfo.ratings}`}
             </Details>
             <Details style={{ fontSize: '12px', color: '#828282' }}>
               {`  (${listingInfo.reviews} reviews)`}
