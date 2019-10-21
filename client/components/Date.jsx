bf81feb47f78875be56d750eb045b43784017447
Faith <pfaithmtan@gmail.com>
2019-10-21 01:10:03 -0700

    Format Date component

--- client/components/Date.jsx
+++ client/components/Date.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import Calendar from './Calendar';
+import styled from 'styled-components';
+
+const DateDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: 'Montserrat', sans-serif;
+`;
+
+const DateBox = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 324px;
+  height: 40px;
+  margin-left: 24px;
+  margin-right: 24px;
+  border: 1px solid rgb(228, 231, 231);
+  border-radius: 1px;
+  font-size: 16px;
+`;
+
+const CheckInOut = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 144px;
+  height: 40px;
+`;
 
 class Date extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       checkIn: false,
@@ -12,7 +40,7 @@ class Date extends React.Component {
     this.handleClickCheckIn = this.handleClickCheckIn.bind(this);
     this.handleClickCheckOut = this.handleClickCheckOut.bind(this);
   }
-  
+
   handleClickCheckIn(event) {
     event.preventDefault();
     console.log('checkIN!');
@@ -22,11 +50,11 @@ class Date extends React.Component {
       checkOut: false,
     });
   }
-  
+
   handleClickCheckOut(event) {
     event.preventDefault();
     console.log('checkOUT!');
-    
+
     this.setState({
       checkOut: true,
       checkIn: false,
@@ -35,21 +63,18 @@ class Date extends React.Component {
 
   render() {
     return (
-      <div>
-        <div onClick={this.handleClickCheckIn}>Check-in</div>
-        -->
-        <div onClick={this.handleClickCheckOut}>Checkout</div>
-      </div>
+      <DateDiv>
+        <div style={{ marginLeft: '24px', fontSize: '12px' }}>Dates</div>
+        <DateBox>
+          {/* <CheckInOut></CheckInOut> */}
+          <CheckInOut onClick={this.handleClickCheckIn}>Check-in</CheckInOut>
+          --&gt;
+          <CheckInOut onClick={this.handleClickCheckOut}>Checkout</CheckInOut>
+          {/* <Calendar /> */}
+        </DateBox>
+      </DateDiv>
     );
   }
 }
-
-// const Date = (props) => (
-//   <div>
-//     <div>Check-in</div>
-//     -->
-//     <div>Checkout</div>
-//   </div>
-// );
 
 export default Date;
