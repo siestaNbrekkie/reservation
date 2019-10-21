4fda9b69467d5111a8fcb35ce5bbdff854fc68e3
Faith <pfaithmtan@gmail.com>
2019-10-21 02:43:11 -0700

    Format reserve button
    - Add styling and hard-code position

--- client/components/ReserveButton.jsx
+++ client/components/ReserveButton.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.input`
+  width: 326px;
+  height: 46px;
+  color: white;
+  background-color: rgb(234,85,92);
+  font-family: 'Montserrat', sans-serif;
+  font-weight: bold;
+  font-size: 16px;
+  border: 1px solid rgb(234,85,92);
+  border-radius: 4px;
+  margin-top: 74px;
+  margin-left: 24px;
+  margin-right: 24px;
+`;
 
 const ReserveButton = (props) => {
   const { handleSubmit } = props;
   return (
     <form onSubmit={handleSubmit}>
-      <input type="submit" value="Reserve" />
+      <Button type="submit" value="Reserve" />
     </form>
   );
 };
