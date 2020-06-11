78bb1445272e45810882c3699f30fa64a72698bb
Faith <pfaithmtan@gmail.com>
2020-06-11 14:45:08 -0700

    Modify schema
    - Remove Calendar model
    - Define Unavailable Dates model
    - Create simple seed for unavailable dates

--- database/index.js
+++ database/index.js
@@ -30,7 +30,7 @@ Listing.init({
     allowNull: false,
   },
   ratings: {
-    type: Sequelize.DECIMAL(10,2),
+    type: Sequelize.DECIMAL(10, 2),
     allowNull: false,
   },
   reviews: {
@@ -55,7 +55,7 @@ Listing.init({
   // options
 });
 
-const Calendar = sequelize.define('calendar', {
+const UnavailableDates = sequelize.define('unavailable_dates', {
   // attributes
   id: {
     type: Sequelize.INTEGER,
@@ -63,385 +63,31 @@ const Calendar = sequelize.define('calendar', {
     allowNull: false,
     primaryKey: true,
   },
-  october1: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
+  day: {
+    type: Sequelize.INTEGER,
   },
-  october2: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
+  month: {
+    type: Sequelize.INTEGER,
   },
-  october3: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october4: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october5: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october6: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october7: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october8: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october9: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october10: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october11: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october12: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october13: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october14: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october15: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october16: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october17: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october18: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october19: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october20: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october21: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october22: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october23: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october24: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october25: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october26: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october27: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october28: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october29: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october30: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  october31: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-
-  november1: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november2: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november3: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november4: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november5: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november6: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november7: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november8: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november9: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november10: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november11: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november12: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november13: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november14: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november15: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november16: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november17: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november18: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november19: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november20: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november21: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november22: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november23: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november24: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november25: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november26: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november27: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november28: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november29: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  november30: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december1: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december2: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december3: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december4: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december5: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december6: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december7: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december8: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december9: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december10: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december11: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december12: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december13: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december14: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december15: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december16: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december17: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december18: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december19: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december20: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december21: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december22: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december23: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december24: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december25: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december26: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december27: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december28: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december29: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december30: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-  december31: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
+  year: {
+    type: Sequelize.INTEGER,
   },
 }, {
   // options
+  indexes: [
+    {
+      unique: true,
+      fields: ['day', 'month', 'year'],
+    },
+  ],
 });
 
 // Associations
-Listing.hasOne(Calendar);
+Listing.hasMany(UnavailableDates);
 
 console.log('testing connection');
 
 module.exports = {
   Listing,
-  Calendar,
+  UnavailableDates,
 };
