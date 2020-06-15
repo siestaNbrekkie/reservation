6178966ebfc507e70009fe0b2258d07f2fe246e1
Faith <pfaithmtan@gmail.com>
2020-06-14 18:49:31 -0700

    Add command to insert dates to database from csv file
    - Note file clears existing db and uses new empty db with no tables
    - Write command directly into mysql shell

--- schema.sql
+++ schema.sql
@@ -2,3 +2,9 @@ DROP DATABASE IF EXISTS reservations;
 CREATE DATABASE reservations;
 
 USE reservations;
+
+LOAD DATA LOCAL INFILE '/Users/Faith/Documents/HRSF123/Immersive/FEC/reservations/database/unavailableDates.csv' 
+INTO TABLE unavailable_dates FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 ROWS 
+(id, listingId, day, month, year);
