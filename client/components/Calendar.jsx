93e9dce9eeaf605428f25c56bb6a955fdac9ed27
Faith <pfaithmtan@gmail.com>
2020-06-15 15:50:58 -0700

    Change colour of arrows

--- client/components/Calendar.jsx
+++ client/components/Calendar.jsx
@@ -92,8 +92,6 @@ class Calendar extends React.Component {
           modifiedDates[`${datesData[i].day}/${datesData[i].month}/${datesData[i].year}`] = false;
         }
 
-        console.log('get dates!!!')
-        console.log('MODIFIED dates ====', modifiedDates)
         this.setState({
           dates: modifiedDates,
         });
@@ -341,7 +339,7 @@ class Calendar extends React.Component {
             <svg width="19" height="19" viewBox="0 0 1000 1000">
               <path
                 d="M 336 275 L 126 485 h 806 c 13 0 23 10 23 23 s -10 23 -23 23 H 126 l 210 210 c 11 11 11 21 0 32 c -5 5 -10 7 -16 7 s -11 -2 -16 -7 L 55 524 c -11 -11 -11 -21 0 -32 l 249 -249 c 21 -22 53 10 32 32 Z"
-                fill="#8C8C8C"
+                fill="#484848"
               />
             </svg>
           </ArrowBox>
@@ -354,7 +352,7 @@ class Calendar extends React.Component {
             <svg width="19" height="19" viewBox="0 0 1000 1000">
               <path
                 d="M694 242l249 250c12 11 12 21 1 32L694 773c-5 5-10 7-16 7s-11-2-16-7c-11-11-11-21 0-32l210-210H68c-13 0-23-10-23-23s10-23 23-23h806L662 275c-21-22 11-54 32-33z"
-                fill="#8C8C8C"
+                fill="#484848"
               />
             </svg>
           </ArrowBox>
