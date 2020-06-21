05ff746821c73ecf8540b33b52c747aebc1051ea
Faith <pfaithmtan@gmail.com>
2020-06-20 17:43:37 -0700

    Init

---
+++ client/components/Modal.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        MODAL
+      </div>
+    );
+  }
+}
+
+export default Modal;
